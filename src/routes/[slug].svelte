21095c3fd1921ef3988bef0d3ed33e601abb7a24
Tomas Oniščiukas <onisciukas.tomas@gmail.com>
2021-12-05 12:39:13 +0200

    feat: get and render pages

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	export const load = async ({ fetch, page: { params } }) => {
+		const { slug } = params;
+		const res = await fetch(`/pages/${slug}.json`);
+		if (res.ok) {
+			const page = await res.json();
+			return { props: { page } };
+		}
+	};
+</script>
+
+<script>
+	export let page;
+	const { title, content } = page;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h1 class="text-4xl font-semibold mb-5">{title}</h1>
+
+<article>
+	{@html content.html}
+</article>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,21 @@
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const res = await fetch('/pages.json');
+		if (res.ok) {
+			const pages = await res.json();
+			return { props: { pages } };
+		}
+	};
+</script>
+
 <script>
 	import '../app.css';
+	import Nav from '../lib/Nav.svelte';
+
+	export let pages;
 </script>
+
+<Nav {pages} />
 
 <main class="container max-w-xl mx-auto px-4">
 	<slot />
